2fb40daba84598f464e0b881b90cc6e6052795f6
DankMadness12 <morriskhaleel6@gmail.com>
2025-11-30 23:26:58 -0400

    Fixed an error in main.py where Pyqt6 Application was not defined

--- Taxi_Booking_System/main.py
+++ Taxi_Booking_System/main.py
@@ -1,9 +1,9 @@
 # This Python file uses the following encoding: utf-8
+from email.mime import application
 import sys
-from PySide6.QtWidgets import QApplication
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = application(sys.argv)
     # ...
     sys.exit(app.exec())
