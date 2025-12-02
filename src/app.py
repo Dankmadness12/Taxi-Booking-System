c99ce040da2cb443095419125c7fa91aee065073
DankMadness12 <morriskhaleel6@gmail.com>
2025-12-01 20:45:13 -0400

    Moved app.py and attempted to link it to the login and register pages

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
+from pydoc import pager
 import tkinter as tk
 from tkinter import ttk
+from typing import Self
+import login
+import register
 
 root = tk.Tk()
 
@@ -12,8 +16,11 @@ root.geometry("500x400")
 root.minsize(500, 400)
 root.maxsize(1000, 500)
 
+#Container Frame
 container = tk.Frame(root)
 container.pack(side="top", fill="both", expand=True)
+
+root.frames = {}
 
 tk.Label(root, text="Taxi Bookings Inc.", font=("Helvetica", 30), anchor="center").pack()
 
@@ -27,6 +34,24 @@ register.pack(padx=20, pady=25)
 register = tk.Button(root, text="Exit", font=("Helvetica", 15), bg="green", fg="black", command=root.quit)
 register.pack(padx=20, pady=25)
 
+for F in (Homepage, login, register):
+    frame = F()
+    root.frames[F] = frame
+    pager.name = F.__name__
+    frame=F(parent=container, controller=root)
+    frame.grid(row=0, column=0, sticky="nsew")
 
+    root.show_frame = Homepage
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class Login(tk.Tk):
+    def __init__(self) -> Self:
+        super().__init__()
+        label = tk.Label(self, text="Login Page", font=("Helvetica", 24))
+        label.pack(pady=20)
 
 root.mainloop()
