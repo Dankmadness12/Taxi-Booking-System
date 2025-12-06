8199392fa618bffbb9766b150b498c9f42565db0
DankMadness12 <morriskhaleel6@gmail.com>
2025-12-06 18:26:54 -0400

    Removed the admin button to manage users and changed the Manage Drivers text to, Assign Driver to Bookings

--- src/app.py
+++ src/app.py
@@ -361,10 +361,8 @@ class Driver(tk.Frame):
 class Admin(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-        ttk.Label(self, text="Admin Page", font=("Helvetica", 18)).pack(pady=20)
-        ttk.Button(self, text="Manage Users").pack(pady=10)
-        ttk.Button(self, text="Manage Drivers").pack(pady=10)
-        ttk.Button(self, text="Manage Bookings").pack(pady=10)
+        ttk.Label(self, text="Welcome Home, Admin", font=("Helvetica", 18)).pack(pady=20)
+        ttk.Button(self, text="Assign Driver to Bookings").pack(pady=10)
         ttk.Button(self, text="Logout", command=lambda: controller.show_frame("Homepage")).pack(pady=10)
         
 #The Bookings Frame <-------------------------------------------------
