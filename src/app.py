aed404e85effaa2ee42b64b9de2a19dd4e3ef945
DankMadness12 <morriskhaleel6@gmail.com>
2025-12-06 19:15:57 -0400

    Changed the Buttons in the Bookings frame to entries

--- src/app.py
+++ src/app.py
@@ -370,10 +370,21 @@ class Bookings(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent)
         ttk.Label(self, text="Book Your Bookings Today!", font=("Helvetica", 18)).pack(pady=20)
-        ttk.Button(self, text="Choose a pickup time").pack(pady=10)
-        ttk.Button(self, text="Choose a pickup location").pack(pady=10)
-        ttk.Button(self, text="Choose a dropoff location").pack(pady=10)
-
+        
+        ttk.Label(self, text="Choose a Date").pack(pady=10)
+        
+        ttk.Label(self, text="Choose a pickup time").pack()
+        self.pickup_time_entry = ttk.Entry(self)
+        self.pickup_time_entry.pack(pady=10)
+        
+        ttk.Label(self, text="Choose a pickup location").pack()
+        self.pickup_location_entry = ttk.Entry(self)
+        self.pickup_location_entry.pack(pady=10)
+        
+        ttk.Label(self, text="Choose a dropoff location").pack()
+        self.dropoff_location_entry = ttk.Entry(self)
+        self.pickup_location_entry.pack(pady=10)
+        
 if __name__ == "__main__":
     app = TaxiBookings()
     app.mainloop()
